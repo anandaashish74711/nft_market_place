5d757bc561604c2c9eb50bfec9d7c99f2a98791f
Aashish Anand <anandaashish512@gmail.com>
2023-10-10 15:52:53 +0530

    save changes

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 
-const nfts = JSON.parse(fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`));
+const filePath = path.join(__dirname, 'Api-starter-file-main', 'nft-data', 'data', 'nft-users.json');
+app.use(express.json());
+let nfts = JSON.parse(fs.readFileSync(filePath));
 
 app.get('/api/v1/device', (req, res) => {
     res.status(200).json({
         status: "success",
-        data: {}
+        data: { nfts }
+    });
+});
+
+app.post('/api/v1/device', (req, res) => {
+    const newId = nfts[nfts.length - 1].id + 1; // Generate a new ID
+    const newNFT = Object.assign({ id: newId }, req.body); // Create a new NFT object
+
+    nfts.push(newNFT); // Add the new NFT to the array
+
+    fs.writeFile(filePath, JSON.stringify(nfts), (err) => {
+        if (err) {
+            return res.status(500).json({
+                status: "error",
+                message: "Error writing to the file"
+            });
+        }
+
+        res.status(201).json({
+            status: "success",
+            nft: newNFT
+        });
     });
 });
 
